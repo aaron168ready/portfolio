c0f00c5d18288a5882b74e14b768ccb74cfc9aa4
FlamingCorsair <aaron168ready@gmail.com>
2026-02-16 11:06:21 -0500

    feat: render demo

--- src/app/pages/layout/background/background.ts
+++ src/app/pages/layout/background/background.ts
@@ -4,12 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-background',
   template: `<div class="pointer-events-none fixed inset-0 overflow-hidden">
     <div
-      class="absolute -top-24 -left-24 size-[520px] rounded-full bg-indigo-500/20 blur-3xl"
+      class="absolute -top-24 -left-24 size-[520px] rounded-full bg-surface-background-indigo blur-3xl"
     ></div>
     <div
-      class="absolute top-1/3 -right-24 size-[520px] rounded-full bg-fuchsia-500/10 blur-3xl"
+      class="absolute top-1/3 -right-24 size-[520px] rounded-full bg-surface-background-fuchsia blur-3xl"
     ></div>
-    <div class="absolute bottom-0 left-1/3 size-[520px] rounded-full bg-cyan-500/10 blur-3xl"></div>
+    <div
+      class="absolute bottom-0 left-1/3 size-[520px] rounded-full bg-surface-background-cyan blur-3xl"
+    ></div>
   </div>`,
 })
 export class BackgroundComponent {}
